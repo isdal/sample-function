d714a9da3b2deb2adc78996d6eb2dc4c0a063609
Steren <steren.giannini@gmail.com>
2018-01-30 17:31:47 +0100

    Update entry point to "function"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.hello = (req, res) => {
+exports.function = (req, res) => {
   if (req.query.name === undefined) {
     res.status(400).send('Please specify a name query parameter. Example ?name=Max');
   } else {
